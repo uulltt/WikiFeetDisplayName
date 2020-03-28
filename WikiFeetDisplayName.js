87d7b777b2da17e19d09ed68b288ed5a2b91d7b9
Aaron <ajvark@gmail.com>
2020-03-28 12:44:08 -0400

    Adding comments to explain stuff

--- WikiFeetDisplayName.js
+++ WikiFeetDisplayName.js
@@ -5,23 +5,17 @@
 // ==/UserScript==
 
 (function(){
-  console.log("HEY!");
-  setInterval(ChangeDisplayName, 1000*15);
-  
-  
+  setInterval(ChangeDisplayName, 1000*15); //runs every 15 seconds ( 15000 milliseconds)  
 }());
 
 function ChangeDisplayName(){
-  console.log("HEY!");
   var Elements = document.getElementsByClassName('r-1qd0xha');
   var i = 0;
-  var displayNames = Array.prototype.slice.call(Elements, 0).filter(e => e.innerHTML == "CUSS BROTHER"); //replace with display name of choice
+  var displayNames = Array.prototype.slice.call(Elements, 0).filter(e => e.innerHTML == "CUSS BROTHER"); //replace with display name of choice. for multiple display names, separate with || (or) brackets (e.innerHTML == "Alice" || e.innterHTML == "Bob")
   for(i = 0; i < displayNames.length; i++){
-      console.log("FOUND IT");
    displayNames[i].style = `background-image: url("https://pbs.twimg.com/media/EUIxwZIXYAAh-ui?format=png&name=small");
 height: 206px;
 display: block;
 width: 588px;`;
-    //displayNames[i].style += "display:inline-block;";
     }
 }
